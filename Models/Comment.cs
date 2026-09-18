986144b6c10e47aee3dd75e2e973e81ee02ed303
KevinRoque1 <kcndacc@gmail.com>
2026-09-18 22:35:39 +0800

    created comment.cs

---
+++ Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolio.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string Author { get; set; } = "";
+        public string Text { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
