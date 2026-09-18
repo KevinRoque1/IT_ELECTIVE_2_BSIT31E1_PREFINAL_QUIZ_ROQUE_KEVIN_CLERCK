e0873d8a21d3f84d3c873ff7ab94b3630a901db6
KevinRoque1 <kcndacc@gmail.com>
2026-09-18 22:35:02 +0800

    created project.cs

---
+++ Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace MyPortfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string GitHubUrl { get; set; } = "";
+        public string ThumbnailPath { get; set; } = "";
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
